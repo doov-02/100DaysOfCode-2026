f6b403e15ccaafb435eb5bda629b8ebffe119b3a
doov-02 <sunstare02@gmail.com>
2026-06-23 22:49:49 +0530

    add Day9 sols

---
+++ DSA/DoorvaAdhikari_590012978/Day 9/question2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void removeSpaces(char *s){
+    int i = 0, j = 0;
+    while(s[i] != '\0'){
+        if(s[i] != ' '){
+            s[j++] = s[i]; }
+        i++; } 
+    s[j] = '\0';
+}
+int main(){
+    char s[100];
+    fgets(s, sizeof(s), stdin);
+    removeSpaces(s);
+    printf("%s", s);
+    return 0;
+}
